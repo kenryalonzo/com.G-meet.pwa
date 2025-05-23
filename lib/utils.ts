1c1897dc72bc5637b7c56e05dd6d5ea2eb3a579b
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-05-23 14:31:28 +0100

    creation of logout and utils for the authentification in the sidebar

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatPathname = (path: string) => {
+  // Remove leading slash and split path segments
+  const segments = path.toLowerCase().split("/").filter(Boolean)
+
+  // Transform segments: replace hyphens with spaces and capitalize words
+  return segments.map((segment) => 
+    segment.split("-")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ")
+  );
+}
